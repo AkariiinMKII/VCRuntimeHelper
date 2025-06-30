292a74d87f384a91420ad66d3cec5d3d6a2ba39d
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-06-30 22:49:15 +0800

    Add package list

---
+++ PackageHashUpdater.ps1
@@ -0,0 +1,22 @@
+$PackageList = Get-Content .\VCRuntimeList.json | ConvertFrom-Json
+
+$TempPath = "$env:TEMP\VCRuntimeHelper"
+if (-Not (Test-Path $TempPath)) {
+    New-Item -Path $TempPath -ItemType Directory | Out-Null
+}
+Get-ChildItem -Path $TempPath -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+
+"x86", "amd64", "arm64", "directx" | ForEach-Object {
+    $PackageList.$_ | ForEach-Object {
+        $FileName = ($_.name, ".exe") -join ""
+        $FilePath = Join-Path -Path $TempPath -ChildPath $FileName
+        if (-Not (Test-Path $FilePath)) {
+            Invoke-WebRequest -Uri $_.url -OutFile $FilePath
+        }
+        $_.hash = ((Get-FileHash $FilePath).Hash).ToLower()
+    }
+}
+
+$PackageList | ConvertTo-Json | Set-Content .\VCRuntimeList.json
+
+Remove-Item -Path $TempPath -Recurse -Force -ErrorAction SilentlyContinue
